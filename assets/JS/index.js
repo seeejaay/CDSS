37b30081eb753b953522b6a989b57b027d5153bd
Carl <carljustinem984@gmail.com>
2024-04-07 18:51:41 +0800

    Updated Index

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,9 +1,17 @@
 const openProf = document.getElementById("_profile");
 const openAssess = document.getElementById("_assessment");
+const openSource = document.getElementById("_source");
 openProf.addEventListener("click", () => {
   window.open("./profile.html", "_self");
 });
 
 openAssess.addEventListener("click", () => {
   window.open("./assessment.html", "_blank");
+});
+
+openSource.addEventListener("click", () => {
+  window.open(
+    "https://www.who.int/health-topics/diabetes#tab=tab_1",
+    "_blank "
+  );
 });
